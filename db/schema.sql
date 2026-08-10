47dec9d3919611d7b619ba52347e5c9d7c9c4512
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-08-10 17:01:04 -0500

    feat: implementação das tabelas no banco de dados

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE task (
+    id INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    data DATE,
+    descricao TEXT
+);
+
+CREATE TABLE recurring_task (
+    id INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    date DATE,
+    descricao TEXT
+);
+
+CREATE TABLE habit (
+    id INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    date DATE,
+    descricao TEXT
+)
