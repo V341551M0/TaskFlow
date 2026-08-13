a217a2429b7110cd438093e845f7889e7c5ca37a
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-08-13 14:01:14 -0500

    style: refatoração do banco de dados

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS task (
     id VARCHAR(64) NOT NULL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    data VARCHAR(64),
+    data DATE,
     todos_os_dias TINYINT(1) NOT NULL DEFAULT 0,
     vezes_ao_dia VARCHAR(64) NOT NULL DEFAULT '1',
     completed_today TINYINT(1) NOT NULL DEFAULT 0,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS task (
 CREATE TABLE IF NOT EXISTS habit (
     id VARCHAR(64) NOT NULL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    data VARCHAR(64),
+    data DATE,
     todos_os_dias TINYINT(1) NOT NULL DEFAULT 0,
     vezes_ao_dia VARCHAR(64) NOT NULL DEFAULT '1',
     completed_today TINYINT(1) NOT NULL DEFAULT 0,
@@ -27,11 +27,18 @@ CREATE TABLE IF NOT EXISTS habit (
 CREATE TABLE IF NOT EXISTS recurring_task (
     id VARCHAR(64) NOT NULL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    data VARCHAR(64),
+    data DATE,
     todos_os_dias TINYINT(1) NOT NULL DEFAULT 0,
     vezes_ao_dia VARCHAR(64) NOT NULL DEFAULT '1',
     completed_today TINYINT(1) NOT NULL DEFAULT 0,
     completion_count INT NOT NULL DEFAULT 0,
     status VARCHAR(64) NOT NULL DEFAULT 'pending'
+);
+
+CREATE TABLE user (
+    id VARCHAR(64) NOT NULL PRIMARY KEY,
+    user VARCHAR(255) NOT NULL,
+    data DATE,
+    senha VARCHAR(64)
 );
 
