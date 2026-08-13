2e921624bcf1a5dfd7ea492372359eace36c6c14
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-08-13 17:52:32 -0500

    style: clean code

--- web/js/pages/logout.js
+++ web/js/pages/logout.js
@@ -10,3 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+
+let card = document.querySelector(".card");
+let loginButton = document.querySelector(".loginButton");
+let cadastroButton = document.querySelector(".cadastroButton");
+
+loginButton.onclick = () => {
+  card.classList.remove("cadastroActive")
+  card.classList.add("loginActive")
+}
+
+cadastroButton.onclick = () => {
+  card.classList.remove("loginActive")
+  card.classList.add("cadastroActive")
+}
